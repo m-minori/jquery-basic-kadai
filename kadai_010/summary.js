955d3b1a2a7fd69fe3a05e032e468e813bf23f8a
m-minori <165997357+m-minori@users.noreply.github.com>
2024-07-05 22:43:16 +0900

    kadai_010 push

---
+++ kadai_010/summary.js
@@ -0,0 +1,22 @@
+$(function(){
+  // 文字色変更
+  $('#change-color').on('click', function(){
+    $('#target').css('color', 'pink');
+  });
+
+  // 文字内容変更
+  $('#change-text').on('click', function(){
+    $('#target').text('Hello!');
+  });
+
+  // フェードアウト
+  $('#fade-out').on('click', function(){
+    $('#target').fadeOut(3500);
+  })
+
+  // フェードイン
+  $('#fade-out').on('click', function(){
+    $('#target').fadeIn(3500);
+  });
+
+});
